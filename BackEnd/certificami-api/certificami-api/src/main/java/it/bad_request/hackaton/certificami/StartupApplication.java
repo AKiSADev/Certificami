4476af70fa99c0ce92fb9e2fcb9dd01871f66c5e
AKiSADev <vivianalamboni@gmail.com>
2020-03-28 17:37:39 +0100

    Pulizia backbone

--- BackEnd/certificami-api/certificami-api/src/main/java/it/bad_request/hackaton/certificami/StartupApplication.java
+++ BackEnd/certificami-api/certificami-api/src/main/java/it/bad_request/hackaton/certificami/StartupApplication.java
@@ -1,12 +1,11 @@
-package it.lombardia.hackaton.certificami;
+package it.bad_request.hackaton.certificami;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
+@EnableAutoConfiguration
 public class StartupApplication {
 
 	public static void main(String[] args) {

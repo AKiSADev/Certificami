91266031242848e7c7520db2659c2bdbcaa2c411
AKiSADev <vivianalamboni@gmail.com>
2020-03-28 23:18:50 +0100

    Get certificazione

--- BackEnd/certificami-api/certificami-api/src/main/java/it/bad_request/hackaton/certificami/rest/CertificazioneRest.java
+++ BackEnd/certificami-api/certificami-api/src/main/java/it/bad_request/hackaton/certificami/rest/CertificazioneRest.java
@@ -3,7 +3,9 @@ package it.bad_request.hackaton.certificami.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.bad_request.hackaton.certificami.dto.RagioniSpostamento;
@@ -11,6 +13,7 @@ import it.bad_request.hackaton.certificami.entity.Certificazione;
 import it.bad_request.hackaton.certificami.service.CertificazioneService;
 
 @RestController
+@RequestMapping("/certificate")
 public class CertificazioneRest {
 
 	@Autowired
@@ -42,6 +45,28 @@ public class CertificazioneRest {
 		
 	}
 	
-	
+	@GetMapping("/getcertificate/{idCert}")
+	public ResponseEntity<Certificazione> getCertificate(Long idCert){
+		
+		String response = "KO";
+		HttpStatus http;
+		
+		try {
+			
+			Certificazione cert = certificazioneService.findCertificazioneById(idCert);
+			
+			if (cert != null) {
+				http = HttpStatus.OK;
+				
+			}else {
+				http = HttpStatus.NOT_FOUND;
+			}
+				 
+			return new ResponseEntity<Certificazione>(cert, http);
+		}catch(Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+	}
 	
 }
--- BackEnd/certificami-api/certificami-api/src/main/java/it/bad_request/hackaton/certificami/service/CertificazioneService.java
+++ BackEnd/certificami-api/certificami-api/src/main/java/it/bad_request/hackaton/certificami/service/CertificazioneService.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Service;
 import it.bad_request.hackaton.certificami.dto.RagioniSpostamento;
 import it.bad_request.hackaton.certificami.entity.Certificazione;
 import it.bad_request.hackaton.certificami.entity.Motivazione;
+import it.bad_request.hackaton.certificami.entity.Utente;
 import it.bad_request.hackaton.certificami.repository.CertificazioneRepository;
 import it.bad_request.hackaton.certificami.repository.MotivazioneRepository;
+import it.bad_request.hackaton.certificami.repository.UtenteRepository;
 import it.bad_request.hackaton.certificami.util.CertificazioneConverter;
 
 @Service
@@ -25,6 +27,9 @@ public class CertificazioneService {
 	@Autowired
 	MotivazioneRepository motivazioneRepository;
 
+	@Autowired
+	UtenteRepository utenteRepository;
+	
 	public Certificazione createCertification(RagioniSpostamento ragioniSpostamento) {
 
 		Certificazione cert = new Certificazione();
@@ -37,7 +42,9 @@ public class CertificazioneService {
 					.findById(Long.parseLong(ragioniSpostamento.getRagioneOption())).orElse(null);
 			cert.setMotivazione(motivazione);
 
-			//		cert.setUtente(utente);
+			Utente utente = utenteRepository.findByUsernameIgnoreCase(ragioniSpostamento.getEmail());
+			
+			cert.setUtente(utente);
 
 			cert.setDataCreazione(LocalDateTime.now(ZoneId.of("Europe/Rome")));
 			cert = certificazioneRepository.save(cert);
@@ -47,5 +54,10 @@ public class CertificazioneService {
 		} catch (Exception e) {
 			return null;
 		}
+	}
+
+	public Certificazione findCertificazioneById(Long idCert) {
+		
+		return certificazioneRepository.findById(idCert).orElse(null);
 	}
 }

1dbc5e0926985d246c0e0e9f3a341668ee015d70
alienpingu <samuele.bertocco@gmail.com>
2020-03-29 10:36:41 +0200

    form html to js

--- FrontEnd/script/script.js
+++ FrontEnd/script/script.js
@@ -1,4 +1,4 @@
-let state = 'login'
+let state = 'core'
 
 fetchLogin = (event) => {
 	let sessionData = {
@@ -6,24 +6,24 @@ fetchLogin = (event) => {
 		psw: document.querySelector('#inputPassword').value
 	}
 
-	fetch('https://example.com/profile', {
-	  method: 'POST', // or 'PUT'
-	  headers: {
-	    'Content-Type': 'application/json',
-	  },
-	  body: JSON.stringify(data),
-	})
-	.then((response) => response.json())
-	.then((data) => {
-	  console.log('Success:', data);
-	})
-	.catch((error) => {
-	  console.error('Error:', error);
-	});
+	// fetch('http://142.93.103.19:8080/api-certificami/user/register', {
+	//   method: 'POST', // or 'PUT'
+	//   headers: {
+	//     'Content-Type': 'application/json',
+	//   },
+	//   body: JSON.stringify(sessionData),
+	// })
+	// .then((response) => response.json())
+	// .then((data) => {
+	//   console.log('Success:', data);
+	// })
+	// .catch((error) => {
+	//   console.error('Error:', error);
+	// });
 	
-	// localStorage.setItem('authSession', JSON.stringify(sessionData.email));
-	// var retrievedObject = localStorage.getItem('authSession')
-	// console.log('authSession: ', JSON.parse(retrievedObject))
+	localStorage.setItem('authSession', JSON.stringify(sessionData.email));
+	var retrievedObject = localStorage.getItem('authSession')
+	console.log('authSession: ', JSON.parse(retrievedObject))
 }
 
 fetchRegister = (event) => {
@@ -35,24 +35,24 @@ fetchRegister = (event) => {
 			psw: document.querySelector('#inputPassword').value
 		}
 
-		fetch('https://example.com/register', {
-		  method: 'POST', // or 'PUT'
-		  headers: {
-		    'Content-Type': 'application/json',
-		  },
-		  body: JSON.stringify(data),
-		})
-		.then((response) => response.json())
-		.then((data) => {
-		  console.log('Success:', data);
-		})
-		.catch((error) => {
-		  console.error('Error:', error);
-		});
+		// fetch('https://example.com/register', {
+		//   method: 'POST', // or 'PUT'
+		//   headers: {
+		//     'Content-Type': 'application/json',
+		//   },
+		//   body: JSON.stringify(sessionData),
+		// })
+		// .then((response) => response.json())
+		// .then((data) => {
+		//   console.log('Success:', data);
+		// })
+		// .catch((error) => {
+		//   console.error('Error:', error);
+		// });
 
-		// localStorage.setItem('authSession', JSON.stringify(sessionData.email));
-		// var retrievedObject = localStorage.getItem('authSession') 
-		// console.log('authSession: ', JSON.parse(retrievedObject))
+		localStorage.setItem('authSession', JSON.stringify(sessionData.email));
+		var retrievedObject = localStorage.getItem('authSession') 
+		console.log('authSession: ', JSON.parse(retrievedObject))
 	}
 	else {
 		console.log('Psw non uguali')
@@ -60,14 +60,64 @@ fetchRegister = (event) => {
 
 }
 
-(state === 'login') ? document.querySelector('#submit').addEventListener('click', e => fetchLogin(e)) 
-: document.querySelector('#submit').addEventListener('click', e => fetchRegister(e));
+
+isValid = () => {
+	document.querySelector('.form-div').classList.add('d-none') 
+	document.querySelector('.core').classList.remove('d-none')
+}
+
+routeChange = (route) => {
+	if (route === 'login') {
+		document.querySelector('#submit').addEventListener('click', e => fetchLogin(e))
+	} else if (route === 'register') {
+		document.querySelector('.title').innerHTML = "Registrazione"
+		document.querySelector('#submit').addEventListener('click', e => fetchRegister(e))
+	} else if (route === 'core'){
+		isValid()
+	}
+	console.log('route: ', route)
+
+}
+
+
+
+
 
 
 document.querySelector('#register').addEventListener('click', () => {
-	document.querySelectorAll('.d-none').forEach(el => el.classList.remove('d-none'))
+	document.querySelectorAll('.register').forEach(el => el.classList.remove('d-none'))
 	document.querySelectorAll('.col-md-6')[0].classList.remove('col-md-6')
 	document.querySelectorAll('.col-md-6')[0].classList.add('d-none')
 	state = 'register'
+	routeChange('register')
 })
 
+
+// Core
+
+
+fetchCode = () => {
+	fetch('https://helloacm.com/api/random/?n=128')
+    .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+        		document.querySelector('.qrskele').style.display = "none"
+                var qrcode = new QRCode(document.getElementById("qrcode"));
+                makeCode = (hash) => qrcode.makeCode(hash)
+                makeCode(data);
+
+				localStorage.setItem('qrCode', JSON.stringify(data));
+				var retrievedObject = localStorage.getItem('qrCode')
+				console.log('qrCode: ', JSON.parse(retrievedObject))
+  		});
+	}
+
+
+var retrievedObject = localStorage.getItem('authSession')
+
+if (retrievedObject) {
+	fetchCode()
+} else {
+	console.log('redirect')
+}
